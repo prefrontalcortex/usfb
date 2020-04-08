6c0df62a73985a532d4dabefeaa9e94c671ab0d7
Vatsal Ambastha <adrenak_@outlook.com>
2020-04-08 18:56:25 +0530

    fix: Reorganise source for platform dependent compilation. Experimental...

--- Assets/StandaloneFileBrowser/Runtime/StandaloneFileBrowser.cs
+++ Assets/StandaloneFileBrowser/Runtime/StandaloneFileBrowser.cs
@@ -1,28 +1,18 @@
 using System;
 
 namespace SFB {
-    public struct ExtensionFilter {
-        public string Name;
-        public string[] Extensions;
-
-        public ExtensionFilter(string filterName, params string[] filterExtensions) {
-            Name = filterName;
-            Extensions = filterExtensions;
-        }
-    }
-
     public class StandaloneFileBrowser {
         private static IStandaloneFileBrowser _platformWrapper = null;
 
         static StandaloneFileBrowser() {
-#if UNITY_STANDALONE_OSX
+#if UNITY_EDITOR
+            _platformWrapper = new StandaloneFileBrowserEditor();
+#elif UNITY_STANDALONE_OSX
             _platformWrapper = new StandaloneFileBrowserMac();
 #elif UNITY_STANDALONE_WIN
             _platformWrapper = new StandaloneFileBrowserWindows();
 #elif UNITY_STANDALONE_LINUX
             _platformWrapper = new StandaloneFileBrowserLinux();
-#elif UNITY_EDITOR
-            _platformWrapper = new StandaloneFileBrowserEditor();
 #endif
         }
 
